efc960873304bcd451055d24869d882e67660632
Yee Peng Chia <yeepeng.chia@gmail.com>
2012-08-28 18:19:14 -0400

    Initial commit.

---
+++ MoviePlayerExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FullscreenVideoTest
+//
+//  Created by Peng on 8/27/12.
+//  Copyright (c) 2012 Cocoa Star Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
